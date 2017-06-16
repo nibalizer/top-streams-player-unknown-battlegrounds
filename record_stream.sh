cee81fad6e52e129ee7a264905b0278884d431e8
Cullen Taylor <cullentaylor@outlook.com>
2017-06-17 01:09:37 +0200

    Modify output file name in record_stream.sh
    The output file name should match the stream name at this point. I had
    left a hardcoded value for the output .mp4 file name in the original
    addition commit for the script.
    
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- record_stream.sh
+++ record_stream.sh
@@ -2,4 +2,4 @@
 
 #TODO: get $stream_name from grab_streams.sh and plumb into this bad boy.
 
-livestreamer twitch.tv/$stream_name best -o disrespect.mp4 & sleep 10; kill $!
+livestreamer "twitch.tv/$stream_name" best -o "$stream_name.mp4" & sleep 10; kill $!
