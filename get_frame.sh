3176ae8f869aee8e868ed673ddcf7055bdc5cf64
Spencer Krum <nibz@spencerkrum.com>
2017-06-16 17:49:52 -0500

    Better get-frame script

--- get_frame.sh
+++ get_frame.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ffmpeg -i video.mp4 -vf select='eq(n\,1)' -vsync vfr -q:v 2 -f image2pipe -vcodec ppm  output.jpg
+ffmpeg -y -i video.mp4 -vf select='eq(n\,1)' -vsync vfr -q:v 2 -f image2pipe -vcodec ppm  output.jpg
 
