9cef44d2b0698ab9c893b77e9d6cabce1c2e27e1
Spencer Krum <nibz@spencerkrum.com>
2017-06-16 18:28:55 -0500

    Make crop/ocr script capable of both 1080p and 720p

--- crop_and_ocr.sh
+++ crop_and_ocr.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 
-for pic in not_playing.png playing_49_alive_russian.png playing_52_alive.png playing_70_alive.png
+for pic in not_playing.png playing_49_alive_russian.png playing_52_alive.png playing_70_alive.png playing_79_alive_720p.jpg
 do
 
 
     echo "${pic}"
-    convert -quiet screenshots/${pic} -crop 45x40+1785+30 target.png
+    vert_pix=$(identify screenshots/${pic} | cut -d " " -f 3 | cut -d "x" -f 2)
+    if [[ $vert_pix = 1080 ]]; then
+        echo "big pic" $vert_pix
+
+        convert -quiet screenshots/${pic} -crop 45x40+1785+30 target.png
+    fi
+    if [[ $vert_pix = 720 ]]; then
+        echo "small pic" $vert_pix
+        convert -quiet screenshots/${pic} -crop 28x20+1190+25 target.png
+    fi
     tesseract -psm 8 target.png out
     cat out.txt
 
