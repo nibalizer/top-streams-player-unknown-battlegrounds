b80307308266a73649e47cbcad3bb6079528216a
Cullen Taylor <cullentaylor@outlook.com>
2017-06-17 12:35:56 +0200

    Merge grab_streams.sh and record_streams.sh
    grab_streams.sh will now pull a list of the top 25 pubg streams and
    record 5s of video from each stream.
    
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- grab_streams.sh
+++ grab_streams.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-function cmd_exists() {
-  type "$1" > /dev/null 2>&1
-}
-
-function main() {
-  streams_endpoint=https://api.twitch.tv/kraken/streams\?game\=PLAYERUNKNOWN\'\S+BATTLEGROUNDS
-
+function ensure_reqs() {
   if [[ -z "$client_id" ]]; then
     echo "Need to set environment variable client_id!"
     exit 1
@@ -17,11 +11,51 @@ function main() {
     exit 1
   fi
 
-  curl -s -H "Client-ID: $client_id"            \
-       $streams_endpoint                        \
-       | jq '.["streams"][]["channel"]["name"]' \
-       | tr -d \"                               \
-       > streams.txt
+  if ! type livestreamer > /dev/null; then
+    echo "Need to install livestreamer (ex: pip install livestreamer)"
+    exit 1
+  fi
+
+  if ! type timeout > /dev/null; then
+    echo "Need to install timeout (ex: apt-get install timeout)"
+    exit 1
+  fi
+}
+
+function grab_streams_list() {
+  local streams_endpoint=https://api.twitch.tv/kraken/streams\?game\=PLAYERUNKNOWN\'\S+BATTLEGROUNDS
+  local streams="$(curl -s -H "Client-ID: $client_id"      \
+                  $streams_endpoint                        \
+                  | jq '.["streams"][]["channel"]["name"]' \
+                  | tr -d \")"
+  echo $streams
+}
+
+function record_stream() {
+  local stream_name="$1"
+
+  timeout -k 0m 5s                                    \
+          livestreamer -Q -f "twitch.tv/$stream_name" \
+          best -o "./stream_clips/$stream_name.mp4"
+}
+
+function main() {
+  ensure_reqs
+  local streams=($(grab_streams_list))
+
+  if [[ -z "$streams" ]]; then
+    echo "There are no live streams at the moment"
+    exit 1
+  fi
+
+  if [[ ! -d ./stream_clips ]]; then
+    echo "Making stream_clips dir..."
+    mkdir ./stream_clips
+  fi
+
+  for stream in "${streams[@]}"; do
+    record_stream $stream
+  done
 }
 
 main "$@"
--- record_stream.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#TODO: get $stream_name from grab_streams.sh and plumb into this bad boy.
-
-livestreamer "twitch.tv/$stream_name" best -o "$stream_name.mp4" & sleep 10; kill $!
